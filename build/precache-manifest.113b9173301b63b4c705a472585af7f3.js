17caaef759fe746c7085de8a5d7ebdc7eb95353e
Francisco Novoa Parada <francisco.novoa.14@sansano.usm.cl>
2020-05-24 19:35:42 -0400

    uibuild

---
+++ build/precache-manifest.113b9173301b63b4c705a472585af7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ead89fcf6000a7e6d8afc30e1c2129e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e1da18511763f171a3",
+    "url": "/static/css/main.35a74a98.chunk.css"
+  },
+  {
+    "revision": "6b4ed26225ace364ff75",
+    "url": "/static/js/2.b4854b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4854b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e1da18511763f171a3",
+    "url": "/static/js/main.a8d1d4ad.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
